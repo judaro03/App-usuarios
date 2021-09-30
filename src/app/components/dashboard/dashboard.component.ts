89f2733ae9e4c4041e054fcbfb92652149211aac
judaro03 <juanrojasb03@gmail.com>
2021-09-30 09:09:35 -0500

    modificaciones usuarios 2

--- src/app/components/dashboard/dashboard.component.ts
+++ src/app/components/dashboard/dashboard.component.ts
@@ -1,15 +1,18 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-dashboard',
   templateUrl: './dashboard.component.html',
-  styleUrls: ['./dashboard.component.css']
+  styleUrls: ['./dashboard.component.css'],
 })
 export class DashboardComponent implements OnInit {
-
-  constructor() { }
+  constructor(private router: Router) {}
 
   ngOnInit(): void {
+    this.inicio();
   }
-
+  inicio() {
+    this.router.navigate(['home']);
+  }
 }
